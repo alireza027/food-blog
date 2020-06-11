887e53075d8bab692285c5b4c5f43651fb42e174
alireza hozhabri <hozhabrialireza27@gmail.com>
2020-06-11 23:40:20 +0430

    add page_404 component

---
+++ client/src/components/Page_404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class Page_404 extends React.Component{
+    render(){
+        return(
+            <div>
+                404
+            </div>
+        )
+    }
+}
+export default Page_404;
