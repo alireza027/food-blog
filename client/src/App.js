17c428def2b8f447fd7addc6984ee151b5c17700
alireza hozhabri <hozhabrialireza27@gmail.com>
2020-06-11 22:25:11 +0430

    create react file and remove something or add something

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        <p>Alireza</p>
+      </div>
+    )
+  }
+}
+export default App;
