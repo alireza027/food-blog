b3a49bc6a0ec285a18c932af1680d7780bfe2b98
alireza hozhabri <hozhabrialireza27@gmail.com>
2020-06-11 23:40:58 +0430

    config router

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,13 @@
 import React from 'react';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import Home from './components/Home';
+import Archive from './components/Archive';
+import Contact from './components/Contact';
+import Single from './components/Single';
+import Static from './components/Static';
+import Login from './components/Login';
+import Register from './components/Register';
+import Page_404 from './components/Page_404';
 class App extends React.Component{
   constructor(){
     super();
@@ -9,9 +18,18 @@ class App extends React.Component{
 
   render(){
     return(
-      <div>
-        <p>App</p>
-      </div>
+      <Router>
+        <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/archive" component={Archive} />
+            <Route path="/single" component={Single} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/static" component={Static} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route component={Page_404} />
+          </Switch>
+      </Router>
     )
   }
 }
