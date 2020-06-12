ff3d4ea3894e23c3605aa4ff9ab49649b436901c
alireza hozhabri <hozhabrialireza27@gmail.com>
2020-06-13 02:47:36 +0430

    add new link(cooking,lifestyle,food)

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,9 @@ import Single from './components/Single';
 import Login from './components/Login';
 import Register from './components/Register';
 import Page_404 from './components/Page_404';
+import Archive_Food from './components/Archive_Food';
+import Archive_Cooking from './components/Archive_Cooking';
+import Archive_Style_Life from './components/Archive_Style_Life';
 class App extends React.Component{
   constructor(){
     super();
@@ -20,6 +23,9 @@ class App extends React.Component{
         <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/archive" exact component={Archive} />
+            <Route path="/archive-food" exact component={Archive_Food} />
+            <Route path="/archive-cooking" exact component={Archive_Cooking} />
+            <Route path="/archive-life-style" exact component={Archive_Style_Life} />
             <Route path="/single" exact component={Single} />
             <Route path="/login" exact component={Login} />
             <Route path="/register" exact component={Register} />
