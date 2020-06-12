2844ad14f55dff7a7e9d3092d654bb0e87392399
alireza hozhabri <hozhabrialireza27@gmail.com>
2020-06-12 21:56:07 +0430

    remove header and footer

--- client/src/components/Page_404.js
+++ client/src/components/Page_404.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import {Link,NavLink} from 'react-router-dom';
-import Header from './Header';
-import Footer from './Footer';
 class Page_404 extends React.Component{
     render(){
         return(
