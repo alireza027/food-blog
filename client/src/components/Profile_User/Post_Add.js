537044be6f07acb0b2dc5daf14517e0f7ace5fe3
alireza hozhabri <hozhabrialireza27@gmail.com>
2020-06-14 21:22:33 +0430

    create and update post add component

---
+++ client/src/components/Profile_User/Post_Add.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import SimpleuploadPlugin from 'ckeditor5-simple-upload/src/simpleupload'
+// import {NavLink,Link} from 'react-router-dom';
+class Post_Add extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+
+        }
+    }
+    render(){
+        return(
+            <>
+                {/* <div>
+                    <h2>CKeditor in react</h2>
+                    <CKEditor
+                    editor={ ClassicEditor }
+                    data=""
+                    
+                />
+                </div> */}
+
+                {/* https://www.youtube.com/watch?v=qjrKVJjTLDc */}
+                {/* after create server */}
+            </>
+        )
+    }
+}
+export default Post_Add
